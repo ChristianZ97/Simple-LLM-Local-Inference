3533e5d53b3ceab1ad34912331befc6f450b02d8
ChristianZ97 <124138449+ChristianZ97@users.noreply.github.com>
2025-02-15 22:24:55 +0800

    Update local_inference_llm.py

--- local_inference_llm.py
+++ local_inference_llm.py
@@ -14,7 +14,7 @@ Example:
     python3 local_inference_llm.py --model meta-llama/Llama-3.2-1B --prompt "How many r's in Strawberry?" --max_length 5000 --quantization int4
     python3 local_inference_llm.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --prompt "How many r's in Strawberry?" --max_length 5000 --quantization fp32
 
-    python3 local_inference_llm.py --model meta-llama/Llama-3.2-3B --prompt "How many r's in Strrawberrier?" --max_length 5000 --quantization fp32
+    python3 local_inference_llm.py --model meta-llama/Llama-3.2-3B --prompt "Think step-by-step. How many r's in Strrawberrier?" --max_length 5000 --quantization fp32
 """
 
 import os
